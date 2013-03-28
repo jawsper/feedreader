f412ed470f366865377fa9c0e4b6a29c6b85f185
Jasper Seidel <sammy@jawsper.nl>
2013-03-28 20:32:29 +0100

    Rewrote favicon loader as View. Cleanup favicon loader logic. Will now not redirect but load the icon manually. (Todo: caching) On error returns default feed icon.

--- feedreader/urls.py
+++ feedreader/urls.py
@@ -4,5 +4,5 @@ from feedreader import views
 urlpatterns = patterns('feedreader.views',
     url( r'^$', 'index', name='index' ),
     url( r'^outline/(?P<outline_id>\d+)/$', 'outline', name='outline' ),
-	url( r'^feed/favicon/(?P<feed_id>\d+)/$', 'feed_favicon', name='feed_favicon' )
+	url( r'^feed/favicon/(?P<feed_id>\d+)/$', views.FeedFaviconView.as_view(), name='feed_favicon' )
 )
--- feedreader/views.py
+++ feedreader/views.py
@@ -5,8 +5,11 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.conf import settings
 
 from feedreader.models import Outline, Feed, Post
+
+import urllib2, urlparse
 
 def outline_to_dict_with_children( outline ):
 	return {
@@ -43,42 +46,79 @@ def outline( request, outline_id ):
 		'posts': posts
 	} )
 
-def feed_favicon( request, feed_id ):
-	import urlparse, urllib2, re
-	
-	feed = Feed.objects.get( pk = feed_id )
-	if feed.faviconUrl:
-		return HttpResponseRedirect( feed.faviconUrl )
-	p_url = urlparse.urlparse( feed.htmlUrl )
-	try:
-		data = urllib2.urlopen( urllib2.Request( feed.htmlUrl, headers={'User-Agent': 'Chrome'} ) ).read()
-		matches = re.findall( '<link ([^>]+)>', data )
-		for match in matches:
-			if re.search( 'rel="(shortcut )?icon"', match ):
-				m = re.search( 'href="([^"]+)"', match )
-				if not m:
-					continue
-				favicon_url = m.groups(1)[0]
-				p_favicon_url = urlparse.urlparse( favicon_url )
-				if not p_favicon_url.hostname:
-					favicon_url = urlparse.urlunparse( p_url[0:2] + p_favicon_url[2:] )
-				try:
-					favicon = urllib2.urlopen( urllib2.Request( favicon_url, headers={'User-Agent': 'Chrome'} ) ).read()
-					if favicon:
-						feed.faviconUrl = favicon_url
-						feed.save()
-						return HttpResponseRedirect( feed.faviconUrl )
-				except:
-					pass
-		favicon_url = '{0}://{1}/favicon.ico'.format( p_url[0], p_url[1] )
+class IconException(Exception):
+	pass
+
+from django.views.generic.base import View
+class FeedFaviconView(View):
+	def get( self, request, feed_id ):
 		try:
-			favicon = urllib2.urlopen( urllib2.Request( favicon_url, headers={'User-Agent': 'Chrome'} ) ).read()
-			if favicon:
-				feed.faviconUrl = favicon_url
-				feed.save()
-				return HttpResponseRedirect( feed.faviconUrl )
+			self.feed = Feed.objects.get( pk = feed_id )
+			if self.feed.faviconUrl:
+				icon = self.load_icon( self.feed.faviconUrl )
+				if icon:
+					return HttpResponse( icon[0], content_type = icon[1] )
+				else:
+					return self.default_icon()
+			
+			# locate the <link> tag and find the icon in there
+			icon = self.find_icon_in_page()
+			if icon:
+				self.save_icon( icon[0] )
+				return HttpResponse( icon[0], content_type = icon[1] )
+			
+			# try <host>/favicon.ico
+			icon = self.try_force_favicon()
+			if icon:
+				self.save_icon( icon[0] )
+				return HttpResponse( icon[0], content_type = icon[1] )
+		except:
+			pass		
+		
+		return self.default_icon()
+	
+	def save_icon( self, url ):
+		self.feed.faviconUrl = url
+		self.feed.save()
+	
+	def load_icon( self, url ):
+		try:
+			result = urllib2.urlopen( urllib2.Request( url, headers = { 'User-Agent': 'Chrome' } ) )
+			data = result.read()
+			content_type = result.headers.get( 'content-type' ) if 'content-type' in result.headers else 'text/html'
+			return ( data, content_type )
+		except:
+			return False
+	
+	def find_icon_in_page( self ):
+		# load associated html page
+		try:
+			data = urllib2.urlopen( urllib2.Request( self.feed.htmlUrl, headers = { 'User-Agent': 'Chrome' } ) ).read()
+			matches = re.findall( '<link ([^>]+)>', data )
+			for match in matches:
+				if re.search( 'rel="(shortcut )?icon"', match ):
+					m = re.search( 'href="([^"]+)"', match )
+					if not m:
+						continue
+				
+					favicon_url = m.groups(1)[0]
+				
+					p_favicon_url = urlparse.urlparse( favicon_url )
+				
+					# relative url, add hostname of site
+					if not p_favicon_url.hostname:
+						p_url = urlparse.urlparse( self.feed.htmlUrl )
+						favicon_url = urlparse.urlunparse( p_url[0:2] + p_favicon_url[2:] )
+				
+					return self.load_icon( favicon_url )
 		except:
 			pass
-	except:
-		pass
-	return HttpResponse( open( '/home/jasper/favicon.ico', 'r' ).read(), content_type = 'image/png' )
+		return False
+	
+	def try_force_favicon( self ):
+		p_url = urlparse.urlparse( self.feed.htmlUrl )
+		return self.load_icon( '{0}://{1}/favicon.ico'.format( p_url[0], p_url[1] ) )
+	
+	def default_icon( self ):
+		return HttpResponse( open( settings.STATIC_ROOT + 'images/icons/silk/feed.png', 'r' ).read(), content_type = 'image/png' )
+
