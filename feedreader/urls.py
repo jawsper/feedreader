68d5daafbd68592cfd02993423d41ec4d9a649c7
Jasper Seidel <tyrion@jawsper.nl>
2013-06-19 15:51:39 +0200

    Added get_unread_counts url

--- feedreader/urls.py
+++ feedreader/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('feedreader.views',
 	url( r'^outline/(?P<outline_id>\d+)/get_data/$', 'get_outline_data', name = 'get_outline_data' ),
 	url( r'^outline/(?P<outline_id>\d+)/set/$', 'outline_set', name = 'outline_set' ),
 	url( r'^outline/(?P<outline_id>\d+)/mark_as_read/', 'outline_mark_as_read', name = 'outline_mark_as_read' ),
+	url( r'^outline_get_unread_counts/', 'get_unread_counts', name = 'get_unread_counts' ),
 	url( r'^feed/(?P<feed_id>\d+)/favicon/$', views.FeedFaviconView.as_view(), name='feed_favicon' ),
 	url( r'^post/(?P<post_id>\d+)/action/(?P<action>[a-z_]+)/$', views.PostActionView.as_view(), name='feed_action' ),
 	url( r'^api/0/(?P<action>[a-z_]+)/$', 'api0', name = 'api0' )
