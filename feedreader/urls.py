220a2e02b2d4a9dc826012c198424531cfa7e6a1
Jasper Seidel <sammy@jawsper.nl>
2013-06-24 14:22:35 +0200

    Changed all api urls to be api/0/*

--- feedreader/urls.py
+++ feedreader/urls.py
@@ -3,15 +3,14 @@ from feedreader import views
 
 urlpatterns = patterns('feedreader.views',
     url( r'^$', 'index', name='index' ),
-	url( r'^get_option/$', 'get_option', name = 'get_option' ),
-	url( r'^set_option/$', 'set_option', name = 'set_option' ),
-    url( r'^outline/(?P<outline_id>\d+)/$', 'outline', name='outline' ),
-	url( r'^outline/(?P<outline_id>\d+)/get_posts/$', 'get_posts', name = 'get_posts' ),
-	url( r'^outline/(?P<outline_id>\d+)/get_data/$', 'get_outline_data', name = 'get_outline_data' ),
-	url( r'^outline/(?P<outline_id>\d+)/set/$', 'outline_set', name = 'outline_set' ),
-	url( r'^outline/(?P<outline_id>\d+)/mark_as_read/', 'outline_mark_as_read', name = 'outline_mark_as_read' ),
-	url( r'^outline_get_unread_counts/', 'get_unread_counts', name = 'get_unread_counts' ),
 	url( r'^feed/(?P<feed_id>\d+)/favicon/$', views.FeedFaviconView.as_view(), name='feed_favicon' ),
-	url( r'^post/(?P<post_id>\d+)/action/(?P<action>[a-z_]+)/$', views.PostActionView.as_view(), name='feed_action' ),
-	url( r'^api/0/(?P<action>[a-z_]+)/$', 'api0', name = 'api0' )
+    url( r'^outline/(?P<outline_id>\d+)/$', 'outline', name='outline' ),
+	url( r'^api/0/get_option/$', 'get_option', name = 'get_option' ),
+	url( r'^api/0/set_option/$', 'set_option', name = 'set_option' ),
+	url( r'^api/0/outline/(?P<outline_id>\d+)/get_posts/$', 'get_posts', name = 'get_posts' ),
+	url( r'^api/0/outline/(?P<outline_id>\d+)/get_data/$', 'get_outline_data', name = 'get_outline_data' ),
+	url( r'^api/0/outline/(?P<outline_id>\d+)/set/$', 'outline_set', name = 'outline_set' ),
+	url( r'^api/0/outline/(?P<outline_id>\d+)/mark_as_read/', 'outline_mark_as_read', name = 'outline_mark_as_read' ),
+	url( r'^api/0/outline_get_unread_counts/', 'get_unread_counts', name = 'get_unread_counts' ),
+	url( r'^api/0/post/(?P<post_id>\d+)/action/(?P<action>[a-z_]+)/$', views.PostActionView.as_view(), name='feed_action' )
 )
