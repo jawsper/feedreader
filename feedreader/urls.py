fad2a9284f3438ff28ec68f88320605258619c0c
Jasper Seidel <sammy@jawsper.nl>
2013-06-24 14:28:46 +0200

    Moved views.py to views/__init__.py; Whitespace changes in urls.py

--- feedreader/urls.py
+++ feedreader/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, url
 from feedreader import views
 
 urlpatterns = patterns('feedreader.views',
-    url( r'^$', 'index', name='index' ),
+	url( r'^$', 'index', name='index' ),
 	url( r'^feed/(?P<feed_id>\d+)/favicon/$', views.FeedFaviconView.as_view(), name='feed_favicon' ),
-    url( r'^outline/(?P<outline_id>\d+)/$', 'outline', name='outline' ),
+	url( r'^outline/(?P<outline_id>\d+)/$', 'outline', name='outline' ),
 	url( r'^api/0/get_option/$', 'get_option', name = 'get_option' ),
 	url( r'^api/0/set_option/$', 'set_option', name = 'set_option' ),
 	url( r'^api/0/outline/(?P<outline_id>\d+)/get_posts/$', 'get_posts', name = 'get_posts' ),
