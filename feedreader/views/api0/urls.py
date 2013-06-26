6436b01b3b937c3ed5ba1f98c6087ebdf6323b95
Jasper Seidel <tyrion@jawsper.nl>
2013-06-26 08:53:20 +0200

    Removed senseless sync url

--- feedreader/views/api0/urls.py
+++ feedreader/views/api0/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns( 'feedreader.views.api0',
-	url( r'^auth/login/$', 'auth.login' ),
 	url( r'^auth/token/$', 'auth.token' ),
-	url( r'^auth/sync/$', 'auth.sync' ),
+	url( r'^auth/login/$', 'auth.login' ),
 	url( r'^outlines/', 'outline.get_all_outlines' ),
 	url( r'^get_options/$', 'get_options' ),
 	url( r'^get_unread_count/$', 'get_unread' ),
