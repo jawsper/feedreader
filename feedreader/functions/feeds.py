990a62df2672628097f74715007ac151548238c1
Jasper Seidel <sammy@jawsper.nl>
2013-06-30 18:05:35 +0200

    Implemented feeds.add_feed function

--- feedreader/functions/feeds.py
+++ feedreader/functions/feeds.py
@@ -2,13 +2,32 @@
 # Author: Jasper Seidel
 # Date: 2013-06-29
 
-from feedreader.models import Feed
+from feedreader.models import Feed, Outline
+import feedparser
 
-def load_feed( feed ):
-	pass
+def load_feed( feedXmlUrl ):
+	data = feedparser.parse( feedXmlUrl )
+	if not data:
+		return
+	if not data.feed:
+		return
+	insert_data = {}
+	if data.feed.title:
+		insert_data['title'] = data.feed.title
+	insert_data['xmlUrl'] = feedXmlUrl
+	if data.feed.link:
+		insert_data['htmlUrl'] = data.feed.link
+	return Feed( **insert_data )
+
 def add_feed( user, feedXmlUrl ):
 	try:
 		feed = Feed.objects.get( xmlUrl = feedXmlUrl )
 	except Feed.DoesNotExist:
 		feed = load_feed( feedXmlUrl )
-	pass
+		feed.save()
+	try:
+		outline = Outline.objects.get( user = user, feed = feed )
+	except Outline.DoesNotExist:
+		outline = Outline( user = user, feed = feed, title = feed.title )
+		outline.save()
+	return { 'outline_id': outline.id }
