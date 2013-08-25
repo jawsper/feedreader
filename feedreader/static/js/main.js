551736cfd39d3c2d90df25b0b858dff9cec0d2e6
Jasper Seidel <sammy@jawsper.nl>
2013-08-25 23:48:39 +0200

    Move javascript to this repo

---
+++ feedreader/static/js/main.js
@@ -0,0 +1,352 @@
+/*
+	File: main.js
+	Copyright: 2013 Jasper Seidel
+	License: MIT
+*/
+
+var base_path = '//' + document.location.host + document.location.pathname;
+
+function api_request( path, args, callback )
+{
+    $.post( get_api_url( path ), args, function( result )
+    {
+        callback( result );
+    });
+}
+
+function get_url( path )
+{
+	//if( base_path.substr( -1 ) != '/' ) base_path += '/';
+	//if( path.substr( 0, 1 ) == '/' ) path = path.substr( 1 );
+	return base_path + path;
+}
+function get_api_url( path )
+{
+	return get_url( 'api/0' + path );
+}
+
+function add_new_feed( url )
+{
+	api_request( '/feed/add/', { url: url }, function( result )
+	{
+		if( result.success )
+		{
+			location.reload();
+		}
+	});
+}
+
+var outline_regex = /^outline-(\d+)$/
+
+$(function(){
+	$('input:submit, button, a.button').button();
+});
+
+$(function()
+{
+	var csrftoken = $.cookie('csrftoken');
+	function csrfSafeMethod(method) {
+		// these HTTP methods do not require CSRF protection
+		return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+	}
+	$.ajaxSetup({
+		crossDomain: false, // obviates need for sameOrigin test
+		beforeSend: function(xhr, settings) {
+			if (!csrfSafeMethod(settings.type)) {
+				xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			}
+		}
+	});
+});
+
+function show_result( data )
+{
+	$('#result_text').text( data.message );
+	if( data.caption ) $('#result_caption_text').text( data.caption );
+	$('#result_caption').toggle( data.caption ? true : false );
+	$('#result_container').toggleClass( 'ui-state-error', data.error ).toggleClass('ui-state-highlight', !data.error);
+	$('#result_icon').toggleClass('ui-icon-alert', data.error).toggleClass('ui-icon-info', !data.error);
+	$('#result').stop(true,true).fadeIn().delay(5000).fadeOut();
+}
+
+function get_unread_counts( outline_id )
+{
+	api_request( '/get_unread_count/', { 'outline_id': outline_id }, function( data )
+	{
+		document.title = data.total > 0 ? 'Feedreader (' + data.total + ')' : 'Feedreader';
+		if( !data.counts ) return;
+		$.each( data.counts, set_unread_count );
+	});
+}
+
+function set_unread_count( outline_id, unread_count )
+{
+	$( '#outline-' + outline_id ).toggleClass( 'has-unread', unread_count > 0 );
+	$( '#outline-unread-count-' + outline_id ).text( unread_count );
+}
+
+var options = 
+{
+	'showOnlyUnread': {
+		'title': 'Show only unread posts',
+		'type': 'boolean',
+		'default': false,
+		'callback': function()
+		{
+			$( '#outlines' ).toggleClass( 'show-only-unread', this['value'] );
+		}
+	}
+};
+
+function load_options()
+{
+	var opts = [];
+	$.each( options, function( k ) { opts.push( k ) } );
+	api_request( '/get_option/', { keys: opts }, function( result )
+	{
+		$.each( options, function( name, data )
+		{
+			if( typeof result.options[ name ] == 'undefined' ) // key not found
+			{
+				data['value'] = data['default'];
+			}
+			else
+			{
+				if( data['type'] == 'boolean' )
+				{
+					data['value'] = result.options[ name ] == 'true';
+				}
+				else
+				{
+					data['value'] = result.options[ name ];
+				}
+			}
+			if( data['callback'] ) data['callback'].apply( data );
+		});
+	} )
+}
+
+function option_button_click( name )
+{	
+	if( options[ name ]['type'] == 'boolean' )
+	{
+		save_option( name, !options[ name ]['value'] );
+	}
+}
+
+function save_option( name, value )
+{
+	var data = {};
+	data[ name ] = value;
+	api_request( '/set_option/', data, function( result )
+	{
+		if( result == 'OK' )
+		{
+			load_options();
+		}
+	});
+}
+
+function on_hash_change()
+{
+	console.debug( window.location.hash );
+	// if not start with #!, ignore
+	if( window.location.hash.substring( 0, 2 ) != '#!' ) return;
+	// get the real url
+	var real_url = window.location.hash.substring( 2 );
+	
+	// if is outline url, then set the outline
+	var match = real_url.match( /\/outline\/(\d+)\// );
+	if( match )
+	{
+		set_outline( match[1] );
+	}
+}
+
+$(function()
+{
+	// make sure to not propagate when clicked to prevent folders from opening/closing
+	$( '#outlines a' ).click( function(e) { e.stopPropagation(); } );
+	
+	$( '#outlines' )
+		.on({ click: function( e ) { e.stopPropagation(); } }, 'a' )
+		.on({
+			click: function()
+			{
+				var outline = $( this ).parent();
+				var outline_id = outline.attr( 'id' ).match( outline_regex )[1];
+				console.debug( outline_id );
+				outline.toggleClass( 'folder-closed' );
+				set_outline_param( outline_id, 'folder_opened', outline.hasClass( 'folder-closed' ) ? 0 : 1, true );
+			}
+		}, '.folder > .outline-line' );
+	
+	/*$( '#outlines > li.folder' ).each(function(k,v)
+	{
+		var t = $(this);
+		$( 'div.outline-line:first', t ).click(function()
+		{
+			t.toggleClass( 'folder-closed' );
+			var outline_id = t.attr( 'id' ).match( outline_regex )[1];
+			console.debug( outline_id );
+			set_outline_param( outline_id, 'folder_opened', t.hasClass( 'folder-closed' ) ? 0 : 1, true );
+		});
+	});*/
+	
+	// make a button for all options
+	$.each( options, function( name, data )
+	{
+		var button = $( '<a>' ).text( data.title ).click( function() { option_button_click( name ); } ).button();
+		$( '#navigation ul.options' ).append( $( '<li>' ).append( button ) );
+	} );
+	// load the options
+	load_options();
+	
+	$( '#new-feed-popup' ).dialog({
+		autoOpen: false,
+		modal: true,
+		buttons: {
+			'Add feed': function()
+			{
+				add_new_feed( $( '#new-feed-url' ).val() );
+			},
+			'Cancel': function()
+			{
+				$( this ).dialog( 'close' );
+			}
+		},
+		close: function()
+		{
+			$( '#new-feed-url' ).val( '' );
+		}
+	});
+	
+	$( '#button-new-feed' ).click( function() { $( '#new-feed-popup' ).dialog( 'open' ); } );
+	// make the options and refresh button work
+	$( '#button-options' ).click( function() { $( '#navigation .options' ).toggle(); } );
+	$( '#button_refresh_page' ).click( function( e ) 
+	{
+		e.preventDefault();
+		//get_unread_counts();
+		load_navigation();
+	});
+	
+	// load unread counts
+	// get_unread_counts();
+	
+	// trigger initial hash change, and set window.hashchange event
+	on_hash_change();
+	$( window ).on( 'hashchange', on_hash_change );
+	
+	load_navigation();
+});
+
+function load_navigation()
+{
+	api_request( '/outlines/', { use_long_keys: 1 }, function( data )
+	{
+		render_navigation( data.outlines );
+	});
+}
+
+function regexp_escape( input )
+{
+	return input.replace( /([\$\{\}])/g, '\\$1' );
+}
+
+function get_template( id )
+{
+	var re_template_keys = /\$\{(?:([a-z]+)\.)*([a-z_]+)\}/g;
+	//var re_keyname = /^\$\{(?:([a-z]+)\.)*([a-z_]+)\}$/;
+	
+	var template = $( id ).text();
+	
+	var match = false;
+	var keys = {};
+	while( ( match = re_template_keys.exec( template, re_template_keys.lastIndex ) ) != null )
+	{
+		var key_name = match[0];
+		keys[ key_name ] = match.splice( 1 );
+	}
+	
+	/*var matched = template.match( re_template_keys );
+	var keys = {};
+	$.each( matched, function( k, v )
+	{
+		var split_key = v.match( re_keyname ); 
+		if( !split_key )
+		{
+			console.debug( v );
+		}
+		else
+		{
+			var key_name = split_key[0];
+			keys[ key_name ] = split_key.splice( 1 );
+		}
+	});*/
+	
+	return { str: template, keys: keys };
+}
+
+function apply_template( template, data_name, data )
+{
+	var str = template.str;
+	$.each( data, function( k, v )
+	{
+		var name = data_name + '.' + k;
+		var rx = RegExp( '\\$\\{' + name + '\\}', 'g' );
+		str = str.replace( rx, v );
+	});
+	return str;
+}
+
+function make_outline( template, outline )
+{
+	var html = $( apply_template( template, 'outline', outline ) );
+	html.addClass( outline.feed_id ? 'feed' : 'folder' );
+	if( !outline.folder_opened )
+	{
+		html.addClass( 'folder-closed' );
+	}
+	html.toggleClass( 'has-unread', outline.unread_count > 0 );
+	if( outline.feed_id )
+	{
+		$( '> .outline-line', html ).css( 'background-image', 'url(' + get_url( 'feed/' + outline.feed_id + '/favicon/' ) + ')' );
+	}
+	return html;
+}
+
+function render_navigation( navigation )
+{
+	var template = get_template( '#templateNavigationItem' );
+	
+	var main_ul = $( '#outlines' );
+	main_ul.empty();
+	$.each( navigation, function( k, outline )
+	{
+		var outline_html = make_outline( template, outline );
+		
+		if( outline.children && outline.children.length > 0 )
+		{
+			var children = $( '<ul>' );
+			$.each( outline.children, function( k, child )
+			{
+				var child = make_outline( template, child );
+				children.append( child );
+			});
+			outline_html.append( children );
+		}
+		main_ul.append( outline_html );
+	});
+}
+
+
+String.prototype.format = function() {
+  var args = arguments;
+  return this.replace(/{(\d+)}/g, function(match, number) { 
+    return typeof args[number] != 'undefined'
+      ? args[number]
+      : match
+    ;
+  });
+};
---
+++ feedreader/static/js/outline.js
@@ -0,0 +1,264 @@
+/*
+	File: outline.js
+	Copyright: 2013 Jasper Seidel
+	Licence: MIT
+*/
+
+/* globals */
+var outline_id = null;
+var current_post = null;
+
+/* directly after load init everything */
+
+$(function()
+{
+	$('body').keypress(function( e )
+	{
+		switch( e.which )
+		{
+			case 'r'.charCodeAt(0):
+				$( '#button_refresh' ).click();
+				break;
+			case 'j'.charCodeAt(0):
+				move_post( +1 );
+				break;
+			case 'k'.charCodeAt(0):
+				move_post( -1 );
+				break;
+			case 'f'.charCodeAt(0):
+				$('body').toggleClass( 'fullscreen' );
+				if( $( 'body' ).hasClass( 'fullscreen' ) ) $( '#content' ).focus();
+				break;
+			case 'm'.charCodeAt(0):
+				if( current_post != null )
+				{
+					var cb = current_post.find( '.footer .action.read' );
+					cb.prop( 'checked', !cb.prop('checked' ) );
+					mark_post_read_state( post_get_id( current_post ), cb.is(':checked') ? 1 : 0 );
+				}
+				break;
+			case 's'.charCodeAt(0):
+				if( current_post != null )
+				{
+					var cb = current_post.find( '.footer .action.starred' );
+					cb.prop( 'checked', !cb.prop('checked' ) );
+					//mark_post_starred_state( post_get_id( current_post ), cb.is(':checked') ? 1 : 0 );
+				}
+				break;
+			case 'v'.charCodeAt(0):
+				if( current_post != null )
+				{
+					window.open( current_post.find( '.link a' ).attr( 'href' ), '_blank' );
+				}
+				break;
+			default: return true; // don't care
+		}
+		//e.preventDefault();
+	});
+	$( '#content' ).scroll( function()
+	{
+		$( '#scroll_pos' ).text( $( '#content' ).scrollTop() );
+	});
+	$( '#button_refresh' ).click( function() { load_outline( outline_id, true ); } );
+	$( '#button_mark_all_as_read' ).click( function() { mark_all_as_read( outline_id ); } );
+	$( '#button_show_only_new' ).click( function() { set_outline_param( outline_id, 'show_only_new' ); } );
+	$( '#button_sort_order' ).click( function() { set_outline_param( outline_id, 'sort_order' ); } );
+});
+
+/* outline functions */
+
+function set_outline( new_outline_id )
+{
+	outline_id = new_outline_id;
+	load_outline( outline_id, false );
+}
+
+function set_outline_param( outline_id, key, value, no_load )
+{
+	if( !outline_id ) return;
+	data = { 'action': key };
+	if( value ) data['value'] = value;
+	
+	$.post( get_api_url( '/outline/' + outline_id + '/set/' ), data, function( result )
+	{
+		if( result == 'OK' )
+		{
+			if( !no_load ) load_outline( outline_id, true );
+		}
+	});
+}
+
+
+function set_outline_data( outline_id, data )
+{
+	$( '#outline_title > a' ).text( data.title ).attr( 'href', data.htmlUrl );
+	$( '#button_show_only_new' ).button( 'option', 'label', data.show_only_new ? data.unread_count + ' new item' + ( data.unread_count != 1 ? 's' : '' ) : 'All items' );
+	$( '#button_sort_order' ).button( 'option', 'label', data.sort_order == 'ASC' ? 'Oldest first' : 'Newest first' );
+	
+	get_unread_counts( outline_id );
+}
+
+function get_outline_data( outline_id )
+{
+	if( !outline_id ) return;
+	$.post( get_api_url( '/outline/' + outline_id + '/get_data/' ), {}, function( data )
+	{
+		set_outline_data( outline_id, data );
+	});
+}
+
+function load_outline( outline_id, forced_refresh )
+{
+	if( !outline_id ) return;
+	$.post( get_api_url( '/outline/' + outline_id + '/get_posts/' ), { forced_refresh: forced_refresh }, function( data )
+	{
+		set_outline_data( outline_id, data );
+		
+		$( '#content' ).scrollTop( 0 );
+		$( '#posts' ).empty();
+		current_post = null;
+		$.each( data.posts, function( k, post )
+		{
+			$( '#posts' ).append( post_build_html( post, data.is_feed ) );
+			post_attach_handlers( post.id );
+		});
+	});
+}
+
+
+function mark_all_as_read( outline_id )
+{
+	if( !outline_id ) return;
+	$.post( get_api_url( '/outline/' + outline_id + '/mark_as_read/' ), {}, function( data )
+	{
+		if( data.success ) load_outline( outline_id, true );
+	});
+}
+
+function load_more_posts( outline_id, skip, on_complete )
+{
+	if( !outline_id ) return;
+	$.post( get_api_url( '/outline/' + outline_id + '/get_posts/' ), { skip: skip }, function( data )
+	{
+		$.each( data.posts, function( k, post )
+		{
+			$( '#posts' ).append( post_build_html( post, data.is_feed ) );
+			post_attach_handlers( post.id );
+		});
+		if( on_complete ) on_complete();
+	});
+}
+
+/* post functions */
+
+function post_get_id( post ) { return post && post.attr('id').replace( /[^\d]/g, '' ); }
+function id_get_post( post_id ) { return $('#post_' + post_id ); }
+
+function mark_post_read_state( post_id, state )
+{
+	set_post_attr_state( post_id, 'read', state );
+	if( !state )
+	{
+		var post = id_get_post( post_id );
+		post.data( 'do-not-auto-mark-read', true );
+	}
+}
+function set_post_starred_state( post_id, state )
+{
+	set_post_attr_state( post_id, 'starred', state );
+}
+function set_post_attr_state( post_id, attr, state )
+{
+	url = get_api_url( '/post/' + post_id + '/action/' + attr + '/' );
+	$.post( url, { state: state }, function( data )
+	{
+		show_result( data );
+		get_outline_data( outline_id );
+	});
+}
+
+function select_post_by_id( post_id )
+{
+	select_post( id_get_post( post_id ) );
+}
+
+function select_post( post )
+{	
+	if( current_post != null && post.length > 0 && current_post.attr( 'id' ) == post.attr( 'id' ) ) return;
+	
+	if( current_post != null ) current_post.removeClass( 'current' );
+	if( post.length == 0 )
+	{
+		current_post = null;
+		return;
+	}
+	current_post = post;
+	current_post.addClass( 'current' );
+	
+	post[0].scrollIntoView( true );
+	
+	if( !post.data( 'do-not-auto-mark-read' ) && !post.find( '.footer .action.read' ).attr( 'checked' ) )
+	{
+		post.find( '.footer .action.read' ).attr( 'checked', true );
+		mark_post_read_state( post_get_id( post ), 1 );
+	}
+}
+
+function post_attach_handlers( post_id )
+{
+	id_get_post( post_id ).click( function()
+	{
+		select_post_by_id( post_id );
+	} )
+	.find( '.footer .action.starred' ).click(function()
+	{
+		set_post_starred_state( post_id, $(this).is(':checked') ? 1 : 0 );
+	}).end()
+	.find( '.footer .action.read' ).click(function()
+	{
+		mark_post_read_state( post_id, $(this).is(':checked') ? 1 : 0 );
+	});
+}
+
+function post_build_html( post, is_feed )
+{
+	var postTemplate = $( "#postTemplate" ).html();
+	var template = postTemplate.format( 
+		post.id,
+		post.link,
+		post.title,
+		post.pubDate,
+		( is_feed ? '' : 'from ' + post.feedTitle + ' ' ) + ( post.author ? 'by <span class="authorName">{0}</span>'.format( post.author ) : '' ),
+		post.content,
+		post.starred ? ' checked="checked"' : '',
+		post.read ? ' checked="checked"' : ''
+	);
+	return template;
+}
+
+function move_post( direction )
+{
+	if( direction > 0 )
+	{
+		if( current_post == null )
+		{
+			select_post( $('#posts .post').first() );
+		}
+		else
+		{
+			var next_post = current_post.next( '.post' );
+			if( next_post.length > 0 ) select_post( next_post );
+			
+			if( current_post.next( '.post' ).length == 0 )
+			{
+				//load_more_posts( outline_id, $('#posts .post').length );
+			}
+		}
+	}
+	else
+	{
+		if( current_post == null ) return; // can't do anything
+		select_post( current_post.prev( '.post' ) );
+	}
+}
+
