17dd794de90e36ba2cbe0552c99e76ab2f211a63
Jasper Seidel <sammy@jawsper.nl>
2013-10-27 17:41:34 +0100

    Move gui urls to gui.urls

---
+++ feedreader_gui/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from django.views.decorators.cache import cache_page
+import feedreader_gui.views as gui
+
+urlpatterns = patterns('feedreader_gui.views',
+	url(r'^$', gui.index, name='index'),
+	url(r'^outline/(?P<outline_id>\d+)/$', gui.outline, name='outline'),
+	url(r'^feed/(?P<feed_id>\d+)/favicon/$', cache_page(60 * 15)(gui.FeedFaviconView.as_view()), name='feed_favicon')
+)
