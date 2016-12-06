f1d81cdf6416e167cfeb7b8266ff0a0fe775bf7f
Jasper Seidel <lenny@jawsper.nl>
2016-12-06 15:50:09 +0100

    Allow external favicon load be disabled.
    It is now possible to define a setting LOAD_FAVICON.
    When False, external favicons will not be loaded and the default image is shown.

--- feedreader_gui/views/__init__.py
+++ feedreader_gui/views/__init__.py
@@ -17,9 +17,9 @@ import urllib.request, urllib.error, urllib.parse
 import os
 
 class SecureDispatchMixIn:
-	@method_decorator(login_required)
-	def dispatch(self, *args, **kwargs):
-		return super().dispatch(*args, **kwargs)
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 class IndexView(SecureDispatchMixIn, TemplateView):
     template_name = 'feedreader/index.html'
@@ -39,44 +39,50 @@ class OutlineView(IndexView):
         return context
 
 class FeedFaviconView(SecureDispatchMixIn, View):
-	def get( self, request, feed_id ):
-		try:
-			feed = Feed.objects.get( pk = feed_id )
-			if feed.faviconUrl:
-				icon = self.load_icon( feed.faviconUrl )
-				if icon:
-					return HttpResponse( icon[0], content_type = icon[1] )
-		except Feed.DoesNotExist:
-			pass
-		
-		return self.default_icon()
-	
-	
-	def load_icon( self, url ):
-		try:
-			result = urllib.request.urlopen( urllib.request.Request( url, headers = { 'User-Agent': 'Chrome' } ) )
-			data = result.read()
-			content_type = result.headers.get( 'content-type' ) if 'content-type' in result.headers else 'text/html'
-			return ( data, content_type, url )
-		except Exception as e:
-			print("Exception in load_icon: {0}".format(e))
-			return False
-	
-	def default_icon( self ):
-		return HttpResponse( open( os.path.join(settings.STATIC_ROOT, 'images/icons/silk/feed.png'), 'rb' ).read(), content_type = 'image/png' )
+    def get( self, request, feed_id ):
+        load_favicon = True
+        try:
+            load_favicon = settings.LOAD_FAVICON
+        except:
+            pass
+        if load_favicon:
+            try:
+                feed = Feed.objects.get( pk = feed_id )
+                if feed.faviconUrl:
+                    icon = self.load_icon( feed.faviconUrl )
+                    if icon:
+                        return HttpResponse( icon[0], content_type = icon[1] )
+            except Feed.DoesNotExist:
+                pass
+        
+        return self.default_icon()
+    
+    
+    def load_icon( self, url ):
+        try:
+            result = urllib.request.urlopen( urllib.request.Request( url, headers = { 'User-Agent': 'Chrome' } ) )
+            data = result.read()
+            content_type = result.headers.get( 'content-type' ) if 'content-type' in result.headers else 'text/html'
+            return ( data, content_type, url )
+        except Exception as e:
+            print("Exception in load_icon: {0}".format(e))
+            return False
+    
+    def default_icon( self ):
+        return HttpResponse( open( os.path.join(settings.STATIC_ROOT, 'images/icons/silk/feed.png'), 'rb' ).read(), content_type = 'image/png' )
 
 class ScriptUrls(TemplateView):
-	template_name = 'feedreader/urls.js.html'
-	content_type = 'application/javascript'
+    template_name = 'feedreader/urls.js.html'
+    content_type = 'application/javascript'
 
-	def get_context_data(self, **kwargs):
-		context = super(ScriptUrls, self).get_context_data(**kwargs)
-		url_dict = {}
-		from django.core.urlresolvers import reverse
-		from django.utils.regex_helper import normalize
-		from feedreader_api.api0 import urls as api_urls
-		for url in api_urls.urlpatterns:
-			url_dict[url.name] = normalize(url.regex.pattern)[0][0]
-		context['urls'] = url_dict
-		#context['latest_articles'] = Article.objects.all()[:5]
-		return context
+    def get_context_data(self, **kwargs):
+        context = super(ScriptUrls, self).get_context_data(**kwargs)
+        url_dict = {}
+        from django.core.urlresolvers import reverse
+        from django.utils.regex_helper import normalize
+        from feedreader_api.api0 import urls as api_urls
+        for url in api_urls.urlpatterns:
+            url_dict[url.name] = normalize(url.regex.pattern)[0][0]
+        context['urls'] = url_dict
+        #context['latest_articles'] = Article.objects.all()[:5]
+        return context
