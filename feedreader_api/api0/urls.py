ab748463af630eddab5c37945434c9355f20bffd
Jasper Seidel <lenny@jawsper.nl>
2016-12-06 17:29:32 +0100

    Move api views to api0.views module.

--- feedreader_api/api0/urls.py
+++ feedreader_api/api0/urls.py
@@ -1,17 +1,16 @@
 from django.conf.urls import url
 
-import feedreader_api.api0 as api0
-from . import auth, outline, post
+from .views import auth, options, outline, post
 
 urlpatterns = [
 	url(r'^auth/token/$', auth.token, name='auth_token'),
 	url(r'^auth/verify/$', auth.verify, name='auth_verify'),
 	url(r'^feed/add/$', outline.add_feed, name='feed_add'),
 	url(r'^outlines/', outline.get_all_outlines, name='outline_get_all_outlines'),
-	url(r'^get_options/$', api0.get_options, name='get_options'),
-	url(r'^get_unread_count/$', api0.get_unread, name='get_unread'),
-	url(r'^get_option/$', api0.get_option, name = 'get_option'),
-	url(r'^set_option/$', api0.set_option, name = 'set_option'),
+	url(r'^get_options/$', options.get_options, name='get_options'),
+	url(r'^get_unread_count/$', options.get_unread, name='get_unread'),
+	url(r'^get_option/$', options.get_option, name = 'get_option'),
+	url(r'^set_option/$', options.set_option, name = 'set_option'),
 	url(r'^outline/get_all_posts/$', outline.get_all_posts, name='get_all_posts'),
 	url(r'^outline/get_starred_posts/$', outline.get_starred_posts, name='get_starred_posts'),
 	url(r'^outline/get_posts/$', outline.get_posts, name='get_posts'),
