f8c3e07e608f7c4a0237ad3f49cec76bbc804080
Jasper Seidel <lenny@jawsper.nl>
2017-03-16 23:40:52 +0100

    Update feedreader.admin

--- feedreader/admin.py
+++ feedreader/admin.py
@@ -4,7 +4,16 @@ from .models import Outline, Feed
 
 @admin.register(Outline)
 class OutlineAdmin(DraggableMPTTAdmin):
+    list_display = (
+        'tree_actions',
+        'indented_title',
+        'feed',
+    )
     list_filter = (
         ('user', admin.RelatedOnlyFieldListFilter),
     )
     list_select_related = ('parent', 'user')
+
+@admin.register(Feed)
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ('title', 'lastUpdated', 'lastStatus', 'disabled')
