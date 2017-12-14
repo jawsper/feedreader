b28f4184881ec1620f173b2afefc4c475a743bac
Jasper Seidel <lenny@jawsper.nl>
2017-12-14 17:50:22 +0100

    Use logging module in feedupdater.

--- feedreader/functions/feedupdate.py
+++ feedreader/functions/feedupdate.py
@@ -10,6 +10,10 @@ import datetime
 import socket
 socket.setdefaulttimeout( 15 )
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 MYSQL_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S%z'
 def compare_datetime_to_struct_time( dt, st ):
     return dt.strftime( MYSQL_DATETIME_FORMAT ) == time.strftime( MYSQL_DATETIME_FORMAT, st )
@@ -55,9 +59,9 @@ class FeedUpdater:
                 feed.save()
 
     def load_feed( self, feed ):
-        if self.stdout:
-            self.stdout.write( '{0:03} {1} '.format( feed.id, feed.xmlUrl ), ending='' )
-            self.stdout.flush()
+        prefix = ' ' * len(f'{feed.id:03}')
+        log_message = f'{feed.id:03} {feed.xmlUrl} '
+        logger.info(log_message)
         args = dict(
             etag=str(feed.lastETag),
             modified=str(feed.lastPubDate if feed.lastPubDate else feed.lastUpdated)
@@ -69,31 +73,24 @@ class FeedUpdater:
 
         if 'status' in data:
             if data['status'] >= 400:
-                if self.stdout:
-                    self.stdout.write( 'Failed: status error {0}'.format( data['status'] ) )
+                logger.warn(f'{prefix}Failed: status error {data["status"]}')
                 return 'Error: {0}'.format( data['status'] )
             elif data.status == 304:
-                if self.stdout:
-                    self.stdout.write('304 Not changed')
+                logger.info(f'{prefix}304 Not Changed')
                 if not self.options.get('force', False):
                     return '304'
 
         if data['bozo']:
-            if self.stdout:
-                self.stdout.write( 'Failed: {0}'.format( data['bozo_exception'] ) )
+            logger.warn(f'{prefix}Failed: {data["bozo_exception"]}')
             return 'Error: {0}'.format( data['bozo_exception'] )
         if not data:
-            if self.stdout:
-                self.stdout.write( 'Failed: no data' )
+            logger.warn(f'{prefix}Failed: no data')
             return 'Error: no data'
 
         if 'etag' in data:
             if feed.lastETag != data.etag:
                 feed.lastETag = data.etag
                 feed.save()
-
-        if self.stdout:
-            self.stdout.write( 'ok!' )
 
         changed = True
         last_updated = None
@@ -113,13 +110,11 @@ class FeedUpdater:
             feed.lastPubDate = time.strftime( MYSQL_DATETIME_FORMAT, last_updated )
 
         if not changed:
-            if self.stdout:
-                self.stdout.write( ' - No changes detected' )
+            logger.info(f'{prefix}No changes detected')
             if not self.options.get('force', False):
                 return None
 
-        if self.stdout:
-            self.stdout.write( ' - scanning {0} posts, please have patience...'.format( len( data['entries'] ) ) )
+        logger.info(f'{prefix}scanning {len(data["entries"])} posts, please have patience...')
 
         imported = 0
 
@@ -146,8 +141,7 @@ class FeedUpdater:
                 if 'link' in entry:
                     insert_data['link'] = entry['link']
                 else:
-                    if self.stdout:
-                        self.stdout.write( 'Can\'t determine a link' )
+                    logger.warn('{prefix}Can\'t find a link.')
                     continue
 
             if 'content' in entry:
@@ -158,8 +152,7 @@ class FeedUpdater:
                 try:
                     insert_data['pubDate'] = time.strftime( MYSQL_DATETIME_FORMAT, entry['published_parsed'] )
                 except TypeError:
-                    if self.stdout:
-                        self.stdout.write( 'Invalid date: {0}'.format( entry['published_parsed'] ) )
+                    logger.warn('{prefix}Invalid date: {entry["published_parsed"]}')
                     continue
             elif 'updated_parsed' in entry and entry['updated_parsed']:
                 insert_data['pubDate'] = time.strftime( MYSQL_DATETIME_FORMAT, entry['updated_parsed'] )
@@ -174,10 +167,7 @@ class FeedUpdater:
                 elif 'description' in insert_data:
                     insert_data['guid'] = insert_data['description']
                 else:
-                    if self.stdout:
-                        self.stdout.write( 'Cannot find a good unique ID' )
-                        self.stdout.write( str( entry ) )
-                        self.stdout.write( str( insert_data ) )
+                    logger.warn(f'{prefix} Cannot find a good unique ID {entry} {insert_data}')
                     raise CommandError( 'See above' )
             if not 'pubDate' in insert_data:
                 insert_data['pubDate'] = datetime.datetime.utcnow().replace(tzinfo=UTC())
@@ -185,9 +175,7 @@ class FeedUpdater:
             try:
                 test = Post.objects.get( guid__exact = insert_data['guid'] )
             except Post.MultipleObjectsReturned:
-                if self.stdout:
-                    self.stdout.write( 'Duplicate post!' )
-                    self.stdout.write( str( insert_data ) )
+                logger.info(f'{prefix}Duplicate post! {insert_data}')
             except Post.DoesNotExist:
                 insert_data['feed'] = feed
                 post = Post( **insert_data )
@@ -199,11 +187,9 @@ class FeedUpdater:
                         UserPost( user = outline.user, post = post ).save() # make userposts for all users who have this feed
                     imported += 1
                 except IntegrityError:
-                    if self.stdout:
-                        self.stdout.write( str( entry ) )
+                    logger.warn(f'{prefix}IntegrityError: {entry}')
                     raise CommandError( 'Invalid post' )
 
-        if self.stdout:
-            self.stdout.write( ' - Inserted {0} new posts'.format( imported ) )
+        logger.info(f'{prefix}Inserted {imported} new posts')
         self.imported += imported
         return 'OK'
