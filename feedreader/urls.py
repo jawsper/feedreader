0ed49e93755e12f2e1e4bf81776d7dafadb4ad2e
Jasper Seidel <lenny@jawsper.nl>
2017-12-24 16:08:53 +0100

    Add urls to feedreader.urls

--- feedreader/urls.py
+++ feedreader/urls.py
@@ -1,6 +1,11 @@
 from django.urls import include, path
+from django.contrib import admin
+import django.contrib.auth.views
 
 urlpatterns = [
-	path('api/', include('feedreader_api.urls')),
-	path('', include('feedreader_gui.urls'))
+    path('feedreader/login/', django.contrib.auth.views.login, name='login'),
+    path('feedreader/logout/', django.contrib.auth.views.logout, name='logout'),
+    path('feedreader/admin/', admin.site.urls),
+    path('feedreader/api/', include('feedreader_api.urls')),
+    path('feedreader/', include('feedreader_gui.urls'))
 ]
