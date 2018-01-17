828475db3a93aad45772d1f4354cbc55f8b8da0f
Jasper Seidel <lenny@jawsper.nl>
2018-01-17 20:53:56 +0100

    Use logging module in update_unread_count

--- feedreader/management/commands/update_unread_count.py
+++ feedreader/management/commands/update_unread_count.py
@@ -1,11 +1,15 @@
+import logging
+
 from django.core.management.base import BaseCommand, CommandError
 
 from feedreader.models import Outline
 from feedreader.functions import get_unread_count
 
+logger = logging.getLogger(__name__)
+
 class Command(BaseCommand):
 	def handle(self, *args, **options):
 		for outline in Outline.objects.all():
-			self.stdout.write('{}: {}'.format(outline.id, str(outline)))
+			logger.info('Updating unread count for {}: {}'.format(outline.id, str(outline)))
 			outline.unread_count = get_unread_count(outline.user, outline)
 			outline.save()
