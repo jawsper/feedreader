399a26d9a6b10672c4878501add90f383152ccd3
Jasper Seidel <code@jawsper.nl>
2018-11-15 17:10:35 +0100

    Display version number in bottom left corner.

--- app/feedreader/__init__.py
+++ app/feedreader/__init__.py
@@ -2,7 +2,7 @@ from .celery import app as celery_app
 
 __all__ = ['celery_app']
 
-__version__ = '1.2.10'
+__version__ = '1.3.1'
 
 def get_version():
     return __version__
---
+++ app/feedreader_gui/templatetags/app_version.py
@@ -0,0 +1,9 @@
+from django import template
+import feedreader
+
+register = template.Library()
+
+
+@register.simple_tag
+def app_version(*args, **kwargs):
+	return feedreader.get_version()
