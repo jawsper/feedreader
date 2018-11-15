8ed668c2a42bae5d2f97c8c880aa2781548d94b3
Jasper Seidel <code@jawsper.nl>
2018-11-15 12:35:40 +0100

    Added script and test settings for testing on travis.

---
+++ app/feedreader/settings/test.py
@@ -0,0 +1,66 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = []
+
+LOAD_FAVICON = False
+
+SECRET_KEY = 'Better generate a better secret key!'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'feedreader',
+        'USER': 'feedreader',
+        'PASSWORD': 'feedreader',
+    }
+}
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[{server_time}] {message}',
+            'style': '{',
+        },
+        'console': {
+            'format': '[{asctime}] - {name} - {levelname} - {message}',
+            'style': '{',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+        'django.server': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'django.server',
+        },
+    },
+    'loggers': {
+        # 'django': {
+        #     'handlers': ['console'],
+        #     'level': 'INFO',
+        # },
+        'django.server': {
+            'level': 'INFO',
+            'handlers': ['django.server'],
+            'propagate': False,
+        },
+        # 'django.db.backends': {
+        #     'level': 'DEBUG',
+        #     'handlers': ['console'],
+        # },
+        'feedreader': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
