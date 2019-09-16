ced7ca1cf13d803335530e997937f24f80c39f93
Jasper Seidel <lenny@jawsper.nl>
2019-09-16 13:19:04 +0200

    Convert tabs to spaces

--- app/feedreader/functions/feeds.py
+++ app/feedreader/functions/feeds.py
@@ -8,29 +8,29 @@ from feedreader import tasks
 from urllib.parse import urlparse
 
 def load_feed(feed_xml_url):
-	hostname = urlparse(feed_xml_url).hostname
-	agent = None
-	if hostname.endswith('.tumblr.com'):
-		agent = 'Mozilla/5.0 (compatible; Baiduspider; +http://www.baidu.com/search/spider.html)'
-	data = feedparser.parse(feed_xml_url, agent=agent)
-	if not data:
-		return
-	if not data.feed:
-		return
-	insert_data = {}
-	if data.feed.title:
-		insert_data['title'] = data.feed.title
-	insert_data['xmlUrl'] = feed_xml_url
-	if data.feed.link:
-		insert_data['htmlUrl'] = data.feed.link
-	return Feed(**insert_data)
+    hostname = urlparse(feed_xml_url).hostname
+    agent = None
+    if hostname.endswith('.tumblr.com'):
+        agent = 'Mozilla/5.0 (compatible; Baiduspider; +http://www.baidu.com/search/spider.html)'
+    data = feedparser.parse(feed_xml_url, agent=agent)
+    if not data:
+        return
+    if not data.feed:
+        return
+    insert_data = {}
+    if data.feed.title:
+        insert_data['title'] = data.feed.title
+    insert_data['xmlUrl'] = feed_xml_url
+    if data.feed.link:
+        insert_data['htmlUrl'] = data.feed.link
+    return Feed(**insert_data)
 
 def add_feed(user, feed_xml_url):
-	try:
-		feed = Feed.objects.get(xmlUrl=feed_xml_url)
-	except Feed.DoesNotExist:
-		feed = load_feed(feed_xml_url)
-		feed.save()
-	outline, created = Outline.objects.get_or_create(user=user, feed=feed, defaults=dict(title=feed.title))
-	tasks.update_feed.delay(feed.pk)
-	return { 'outline_id': outline.id }
+    try:
+        feed = Feed.objects.get(xmlUrl=feed_xml_url)
+    except Feed.DoesNotExist:
+        feed = load_feed(feed_xml_url)
+        feed.save()
+    outline, created = Outline.objects.get_or_create(user=user, feed=feed, defaults=dict(title=feed.title))
+    tasks.update_feed.delay(feed.pk)
+    return { 'outline_id': outline.id }
