1047a43b16f8383eb033dc2a7a08c19fc8279716
Jasper Seidel <lenny@jawsper.nl>
2019-09-18 10:58:26 +0200

    Allow JsonResponseView to get POST data as JSON

--- app/feedreader_api/functions.py
+++ app/feedreader_api/functions.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse
 from django.views.generic.base import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.conf import settings
+from json import loads
 
 class JsonResponseView(LoginRequiredMixin, View):
     raise_exception = True
@@ -10,4 +11,9 @@ class JsonResponseView(LoginRequiredMixin, View):
         raise NotImplementedError
 
     def post(self, request):
-        return JsonResponse(self.get_response(request.user, request.POST))
+        if request.method == 'POST' and request.content_type == 'application/json':
+            encoding = request.encoding if request.encoding is not None else settings.DEFAULT_CHARSET
+            request_body = loads(request.body.decode(encoding))
+        else:
+            request_body = request.POST
+        return JsonResponse(self.get_response(request.user, request_body))
