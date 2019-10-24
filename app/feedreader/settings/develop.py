da9a801e67c22f0b2f0a3496bd4697a51f2300f1
Jasper Seidel <lenny@jawsper.nl>
2019-10-24 16:29:14 +0200

    Add debug toolbar to develop settings

--- app/feedreader/settings/develop.py
+++ app/feedreader/settings/develop.py
@@ -16,6 +16,35 @@ DATABASES = {
     }
 }
 
+INSTALLED_APPS += ('debug_toolbar',)
+MIDDLEWARE = ( 'debug_toolbar.middleware.DebugToolbarMiddleware', ) + MIDDLEWARE
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+    '::1',
+]
+
+DEBUG_TOOLBAR_PANELS = [
+    'ddt_request_history.panels.request_history.RequestHistoryPanel',  # Here it is
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'RESULTS_CACHE_SIZE': 100,
+    'RESULTS_STORE_SIZE': 100,
+}
 
 LOGGING = {
     'version': 1,
