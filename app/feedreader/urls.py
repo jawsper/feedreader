f365982706a49d12b6206e9c4d857308b4aad6a3
Jasper Seidel <lenny@jawsper.nl>
2019-10-24 16:27:48 +0200

    Add debug mode URLs for static/media/debug-toolbar

--- app/feedreader/urls.py
+++ app/feedreader/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from django.contrib import admin
 import django.contrib.auth.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('login/', django.contrib.auth.views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
@@ -8,4 +10,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('feedreader_api.urls')),
     path('', include('feedreader_gui.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
