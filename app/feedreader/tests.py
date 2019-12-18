314b9aa95c1fc387b739e88455ef9940a8249d55
Jasper Seidel <jasper@jawsper.nl>
2019-12-18 15:32:54 +0100

    Add some actual tests.

--- app/feedreader/tests.py
+++ app/feedreader/tests.py
@@ -6,11 +6,43 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from feedreader.models import Post
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class PostContentTest(TestCase):
+    def test_relative_url(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that relative URLs are replaced by absolute URLs.
         """
-        self.assertEqual(1 + 1, 2)
+        post = Post()
+        post.link = "https://example.org"
+        post.content = """<a href="/url">url</a>"""
+
+        self.assertEqual(post.processed_content, """<a href="https://example.org/url">url</a>""")
+
+    def test_iframe_replacement(self):
+        """
+        Tests that iframes are replaced by a link to the source.
+        """
+        post = Post()
+        post.content = """<iframe src="https://example.org"></iframe>"""
+
+        self.assertEqual(post.processed_content, """<a href="https://example.org">iframe</a>""")
+
+    def test_iframe_no_src(self):
+        """
+        Tests that iframes with no src attribute get removed.
+        """
+        post = Post()
+        post.content = """<iframe></iframe>"""
+
+        self.assertEqual(post.processed_content, "")
+
+    def test_iframe_removes_script(self):
+        """
+        Tests that scripts are removed from content.
+        """
+        post = Post()
+        post.content = """<script src="https://example.org/script.js"></script>"""
+
+        self.assertEqual(post.processed_content, "")
