489ec8e4487af8fce9ff6aa51bda03b1a97bbab4
Jasper Seidel <code@jawsper.nl>
2022-06-06 20:53:24 +0200

    Call endpoints with axios instead of fetch
    This helps by automatically throwing exceptions if status code >= 400

--- app/feedreader_gui/assets/js/api/base.ts
+++ app/feedreader_gui/assets/js/api/base.ts
@@ -1,31 +1,28 @@
+import axios, { AxiosError } from "axios";
 import Cookies from "js-cookie";
 
 import { toasts } from "../stores";
 
 const urls = JSON.parse(document.getElementById("urls").textContent);
 
-export async function api_request<T>(path: string, args: any = null): Promise<T> {
-  let fetch_args: RequestInit = {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      "X-CSRFToken": Cookies.get("csrftoken"),
-    },
-    credentials: "include",
-  };
-  if (args) {
-    fetch_args["body"] = JSON.stringify(args);
-  }
+export async function api_request<T>(
+  path: string,
+  args: any = null
+): Promise<T> {
   try {
-    const response = await fetch(urls[path].url, fetch_args);
-    return response.json();
+    const response = await axios.post<T>(urls[path].url, args, {
+      headers: {
+        "X-CSRFToken": Cookies.get("csrftoken"),
+      },
+    });
+    return response.data;
   } catch (error) {
+    const axios_error = error as AxiosError;
     toasts.push({
-      caption: "Error",
-      message: error,
+      caption: `Error in request ${path}`,
+      message: axios_error.message,
       success: false,
     });
     throw error;
   }
-};
+}
