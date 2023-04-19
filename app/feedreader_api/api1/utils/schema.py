3c3ece2d0f72b12612dc72f5cff0446692f27fcf
Jasper Seidel <1558353+jawsper@users.noreply.github.com>
2023-04-19 11:50:43 +0200

    fix: make sure to pass kwargs to view to make sure overriden serializers are used

--- app/feedreader_api/api1/utils/schema.py
+++ app/feedreader_api/api1/utils/schema.py
@@ -1,8 +1,14 @@
+import re
+
 from rest_framework.schemas.openapi import AutoSchema as BaseAutoSchema
 from rest_framework_recursive.fields import RecursiveField
 
 
 class AutoSchema(BaseAutoSchema):
+    def __init__(self, tags=None, operation_id_base=None, component_name=None):
+        super().__init__(tags, operation_id_base, component_name)
+        self.regex = re.compile("{(.+)}")
+
     def map_field(self, field):
         if isinstance(field, RecursiveField) and field.proxied:
             if getattr(field.proxied, "many", False):
@@ -12,3 +18,9 @@ class AutoSchema(BaseAutoSchema):
                 }
             return self.get_reference(field.proxied)
         return super().map_field(field)
+
+    def get_serializer(self, path, method):
+        if "{" in path:
+            kwargs = self.regex.findall(path)
+            self.view.kwargs = {kwarg: 0 for kwarg in kwargs}
+        return super().get_serializer(path, method)
