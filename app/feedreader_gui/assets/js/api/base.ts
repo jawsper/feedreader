5169e814a86cc5c0950cdf6eb59dfaff19bccb30
Jasper Seidel <1558353+jawsper@users.noreply.github.com>
2023-04-20 10:05:14 +0200

    feat(gui): Add API instance

--- app/feedreader_gui/assets/js/api/base.ts
+++ app/feedreader_gui/assets/js/api/base.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError } from "axios";
 import Cookies from "js-cookie";
 
 import { toasts } from "../stores";
+import { ApiApi, Configuration } from "./gen/";
 
 const urls = JSON.parse(document.getElementById("urls").textContent);
 
@@ -26,3 +27,11 @@ export async function api_request<T>(
     throw error;
   }
 }
+
+const configuration = new Configuration({
+  basePath: location.origin,
+  headers: {
+    "X-CSRFToken": Cookies.get("csrftoken"),
+  }
+})
+export const api = new ApiApi(configuration);
