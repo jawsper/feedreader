97191220616fec8a203d8e1fbe384ac96a7cb973
Jasper Seidel <1558353+jawsper@users.noreply.github.com>
2023-04-21 22:24:41 +0200

    feat: add middleware to show toasts when requests fail

--- app/feedreader_gui/assets/js/api/base.ts
+++ app/feedreader_gui/assets/js/api/base.ts
@@ -7,6 +7,26 @@ const configuration = new Configuration({
   basePath: location.origin,
   headers: {
     "X-CSRFToken": Cookies.get("csrftoken"),
-  }
-})
+  },
+  middleware: [
+    {
+      post: async (context) => {
+        if (context.response.status >= 400) {
+          toasts.push({
+            caption: `Error in request ${context.url}`,
+            message: `Status code: ${context.response.status}`,
+            success: false,
+          });
+        }
+      },
+      onError: async (context) => {
+        toasts.push({
+          caption: `Exception on request ${context.url}`,
+          message: `${context.error}`,
+          success: false,
+        });
+      },
+    },
+  ],
+});
 export const api = new ApiApi(configuration);
