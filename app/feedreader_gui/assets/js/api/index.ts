34d93b445dd189a8ee9fef911114363fc598d98e
Jasper Seidel <1558353+jawsper@users.noreply.github.com>
2023-04-21 11:31:22 +0200

    fix: remove unneeded export

--- app/feedreader_gui/assets/js/api/index.ts
+++ app/feedreader_gui/assets/js/api/index.ts
@@ -9,5 +9,3 @@ export { load_posts, load_more_posts, set_post_attr_state } from "./posts";
 export { update_config } from "./config";
 
 export { add_new_feed } from "./feed";
-
-export * from "./gen/models";
