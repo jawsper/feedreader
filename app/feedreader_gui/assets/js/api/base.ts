109d28e9886a71c44abd4fa589fed072953cb678
Jasper Seidel <1558353+jawsper@users.noreply.github.com>
2023-04-21 11:26:27 +0200

    feat: remove api_request function

--- app/feedreader_gui/assets/js/api/base.ts
+++ app/feedreader_gui/assets/js/api/base.ts
@@ -1,32 +1,7 @@
-import axios, { AxiosError } from "axios";
 import Cookies from "js-cookie";
 
 import { toasts } from "../stores";
 import { ApiApi, Configuration } from "./gen/";
-
-const urls = JSON.parse(document.getElementById("urls").textContent);
-
-export async function api_request<T>(
-  path: string,
-  args: any = null
-): Promise<T> {
-  try {
-    const response = await axios.post<T>(urls[path].url, args, {
-      headers: {
-        "X-CSRFToken": Cookies.get("csrftoken"),
-      },
-    });
-    return response.data;
-  } catch (error) {
-    const axios_error = error as AxiosError;
-    toasts.push({
-      caption: `Error in request ${path}`,
-      message: axios_error.message,
-      success: false,
-    });
-    throw error;
-  }
-}
 
 const configuration = new Configuration({
   basePath: location.origin,
