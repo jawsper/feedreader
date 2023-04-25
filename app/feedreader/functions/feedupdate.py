9052f158285ca2a415613178e830478822a80f0a
Jasper Seidel <1558353+jawsper@users.noreply.github.com>
2023-04-25 10:00:28 +0200

    fix: don't throw error on bozo
    https://pythonhosted.org/feedparser/bozo.html
    bozo does not mean failed to parse fully, but rather failed to parse strictly

--- app/feedreader/functions/feedupdate.py
+++ app/feedreader/functions/feedupdate.py
@@ -182,8 +182,7 @@ class FeedUpdater:
             self.log.warning("Failed: no data")
             raise FeedUpdateFailure("Error | No data")
         if data["bozo"]:
-            self.log.warning("Failed to parse XML: %s", data["bozo_exception"])
-            raise FeedUpdateFailure("Failed to parse response as XML")
+            self.log.warning("Failed to parse XML strictly: %s", data["bozo_exception"])
 
         if etag := response.headers.get("etag"):
             if self.feed.last_etag != etag:
