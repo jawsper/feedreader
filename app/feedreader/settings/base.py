b2787daa0d601f01e3b391685364c1b264d73fa5
Jasper Seidel <1558353+jawsper@users.noreply.github.com>
2023-05-07 23:32:02 +0200

    feat: remove X-Frame-Options middleware

--- app/feedreader/settings/base.py
+++ app/feedreader/settings/base.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
