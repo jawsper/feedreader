ccae78860add818e37882feddb530f11455dd611
Jasper Seidel <1558353+jawsper@users.noreply.github.com>
2023-06-29 18:32:12 +0200

    feat: only import debug_toolbar if it's installed

--- app/feedreader/settings/develop.py
+++ app/feedreader/settings/develop.py
@@ -19,8 +19,14 @@ DATABASES = {
     }
 }
 
-INSTALLED_APPS += ["debug_toolbar"]
-MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
+try:
+    import debug_toolbar  # noqa
+
+    INSTALLED_APPS += ["debug_toolbar"]
+    MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
+
+except ImportError:
+    pass
 
 INTERNAL_IPS = [
     "127.0.0.1",
